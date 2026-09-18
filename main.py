889f73d3757fccec2b80b0290b32851af1528067
Eduardo <eduessig@furb.br>
2026-09-18 20:52:39 -0300

    refactor: improve agent movement and perception logic in vacuum simulation

--- main.py
+++ main.py
@@ -1,74 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import random
-
-from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
 
 matriz = np.zeros((6, 6), dtype=int)
 
 matriz[0, :] = 1
-matriz[-1, :] = 1
+matriz[5, :] = 1
 matriz[:, 0] = 1
-matriz[:, -1] = 1
-
-qtde_vezes = random.randint(3, 7)
+matriz[:, 5] = 1
 
 posicao_coluna = random.randint(1, 4)
 posicao_linha = random.randint(1, 4)
 
-for i in range(qtde_vezes):
+qtde_vezes = random.randint(3, 7)
+
+for _ in range(qtde_vezes):
     linha = random.randint(1, 4)
     coluna = random.randint(1, 4)
 
     matriz[linha, coluna] = 2
 
-cores = ListedColormap([
-    "white",
-    "orange",
-    "black"
-])
-
-plt.imshow(matriz, cmap=cores)
-
-plt.xticks(np.arange(-0.5, 6, 1), [])
-plt.yticks(np.arange(-0.5, 6, 1), [])
-
-plt.grid(color="black", linewidth=2)
-
-plt.plot(
-    posicao_coluna,
-    posicao_linha,
-    marker="o",
-    markersize=30,
-    color="red"
-)
-
-plt.show()
-
-
-def descer():
-    global posicao_linha
-    posicao_linha += 1
-
-
-def subir():
-    global posicao_linha
-    posicao_linha -= 1
-
-
-def moverDireita():
-    global posicao_coluna
-    posicao_coluna += 1
-
-
-def moverEsquerda():
-    global posicao_coluna
-    posicao_coluna -= 1
-
-
-def aspirar():
-    matriz[posicao_linha, posicao_coluna] = 0
-
+voltandoAoInicio = False
 
 def funcaoMapear():
     posicao = matriz[posicao_linha, posicao_coluna]
@@ -80,8 +32,46 @@ def funcaoMapear():
 
     return sujeira
 
+def descer():
+    global posicao_linha
+
+    if posicao_linha < 4:
+        posicao_linha += 1
+
+def subir():
+    global posicao_linha
+
+    if posicao_linha > 1:
+        posicao_linha -= 1
+
+def moverDireita():
+    global posicao_coluna
+
+    if posicao_coluna < 4:
+        posicao_coluna += 1
+
+def moverEsquerda():
+    global posicao_coluna
+
+    if posicao_coluna > 1:
+        posicao_coluna -= 1
+
+def aspirar():
+    matriz[posicao_linha, posicao_coluna] = 0
 
 def movimentarAgente():
+    global voltandoAoInicio
+
+    if posicao_linha == 4 and posicao_coluna == 1:
+        voltandoAoInicio = True
+
+    if voltandoAoInicio:
+        if posicao_linha > 1:
+            subir()
+        else:
+            voltandoAoInicio = False
+        return
+
     if posicao_linha % 2 == 1:
         if posicao_coluna == 4:
             descer()
@@ -93,7 +83,6 @@ def movimentarAgente():
         else:
             moverEsquerda()
 
-
 def agenteReativoSimples(percepcao):
     sujeira = percepcao
 
@@ -101,3 +90,55 @@ def agenteReativoSimples(percepcao):
         aspirar()
     else:
         movimentarAgente()
+
+cores = [
+    'white',
+    'orange',
+    'black'
+]
+
+fig, ax = plt.subplots()
+
+imagem = ax.imshow(
+    matriz,
+    cmap=plt.cm.colors.ListedColormap(cores)
+)
+
+ax.set_xticks(np.arange(-0.5, 6, 1), minor=True)
+ax.set_yticks(np.arange(-0.5, 6, 1), minor=True)
+
+ax.grid(
+    which='minor',
+    color='gray',
+    linestyle='-',
+    linewidth=1
+)
+
+ax.set_xlim(-0.5, 5.5)
+ax.set_ylim(5.5, -0.5)
+
+bolinha, = ax.plot(
+    [posicao_coluna],
+    [posicao_linha],
+    'ro',
+    markersize=15
+)
+
+plt.ion()
+
+while True:
+    percepcao = funcaoMapear()
+
+    agenteReativoSimples(percepcao)
+
+    imagem.set_data(matriz)
+
+    bolinha.set_data(
+        [posicao_coluna],
+        [posicao_linha]
+    )
+
+    fig.canvas.draw_idle()
+    fig.canvas.flush_events()
+
+    plt.pause(0.5)
